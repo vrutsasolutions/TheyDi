edd95ece4b743391cc892cbd3779e3b9e79d99ec
somi-oss <code.create08@gmail.com>
2026-08-06 12:05:25 +0530

    Rename package to com.theydi.app, fix Google Sign-In, fix duplicate push notifications

---
+++ android/app/src/main/kotlin/com/theydi/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.theydi.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/theydi/theydi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.theydi.theydi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
